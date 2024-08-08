7d84ff985c6175f5da4428d076fabb99d45ff9bc
Damian Barrous-Dume <damianbarrous@gmail.com>
2024-08-08 12:51:39 +0100

    Remove unused util

--- padre_meddea/util/util.py
+++ padre_meddea/util/util.py
@@ -3,9 +3,3 @@ This module provides general utility functions.
 """
 
 __all__ = ["VALID_DATA_LEVELS"]
-
-TIME_FORMAT_L0 = "%Y%j-%H%M%S"
-TIME_FORMAT = "%Y%m%dT%H%M%S"
-VALID_DESCRIPTORS = ["eventlist", "spec-eventlist", "spec", "xraydirect"]
-VALID_DATA_LEVELS = ["l0", "l1", "l2", "l3", "l4"]
-FILENAME_EXTENSION = ".fits"
