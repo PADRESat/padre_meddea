08501b46e586d45db4f0e17b3b1a6a3f9e9ba1aa
Steven Christe <ehsteve@users.noreply.github.com>
2024-11-18 08:15:18 -0500

    Update calibration.py

--- padre_meddea/calibration/calibration.py
+++ padre_meddea/calibration/calibration.py
@@ -80,6 +80,7 @@ def process_file(filename: Path, overwrite=False) -> list:
             hdul = fits.HDUList([empty_primary_hdu, hit_hdu, pkt_hdu])
 
             path = create_science_filename(
+                'meddea',
                 time=primary_hdr["DATE-BEG"],
                 level="l1",
                 descriptor="eventlist",
