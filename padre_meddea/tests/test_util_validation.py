b977624acfa523d1b51a80ad3e1fc761caf948d9
Andrew Robbertz <24920994+Alrobbertz@users.noreply.github.com>
2025-03-11 15:15:35 -0400

    Enable File Validation (#34)
    * Enable File Validation
    
    * Add PADRE Validation Abstraction Layer
    
    * Linting
    
    * Add Custom Validation Function & Unit Tests

---
+++ padre_meddea/tests/test_util_validation.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import pytest
+
+import padre_meddea
+from padre_meddea.util import validation
+
+
+def test_validate_packet_checksums():
+    test_file = padre_meddea._test_files_directory / "apid160_4packets.bin"
+    warnings = validation.validate_packet_checksums(test_file)
+    assert len(warnings) == 0
+
+
+def test_validate():
+    test_file = padre_meddea._test_files_directory / "apid160_4packets.bin"
+    warnings = validation.validate(test_file)
+    assert len(warnings) == 0
