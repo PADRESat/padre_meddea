c084f4e88cb4496ef4eab7fe2bfe51b2db47577d
Steven Christe <ehsteve@users.noreply.github.com>
2025-07-11 13:20:45 -0400

    Calibrate temperatures before sending to timeseries (#66)
    * remove bad cols and calibrating others
    
    * black fix
    
    * bug fix

--- padre_meddea/io/aws_db.py
+++ padre_meddea/io/aws_db.py
@@ -7,6 +7,7 @@ from swxsoc.util.util import record_timeseries, create_annotation
 
 from padre_meddea import log
 import padre_meddea.util.util as util
+from padre_meddea.housekeeping.calibration import get_calibration_func
 
 
 def record_spectra(pkt_ts, spectra, ids):
@@ -39,10 +40,32 @@ def record_photons(pkt_list, event_list):
     create_annotation(pkt_list.time[0], f"{pkt_list.meta['ORIGFILE']}", ["meta"])
 
 
-def record_housekeeping(hk_ts):
+def record_housekeeping(hk_ts: TimeSeries):
     """Send the housekeeping time series to AWS."""
-    record_timeseries(hk_ts, "housekeeping", "meddea")
-    create_annotation(hk_ts.time[0], f"{hk_ts.meta['ORIGFILE']}", ["meta"])
+    my_hk_ts = hk_ts.copy()
+    colnames_to_remove = [
+        "CCSDS_APID",
+        "CCSDS_VERSION_NUMBER",
+        "CCSDS_PACKET_TYPE",
+        "CCSDS_SECONDARY_FLAG",
+        "CCSDS_SEQUENCE_FLAG",
+        "CCSDS_SEQUENCE_COUNT",
+        "CCSDS_PACKET_LENGTH",
+        "timestamp",
+        "CHECKSUM",
+    ]
+    for this_col in colnames_to_remove:
+        if this_col in hk_ts.colnames:
+            my_hk_ts.remove_column(this_col)
+    # calibrate hard to calibrate columns before sending
+    colnames_to_calibrate = ["fp_temp", "hvps_temp", "dib_temp"]
+    for this_col in colnames_to_calibrate:
+        if this_col in hk_ts.colnames:
+            f = get_calibration_func(this_col)
+            my_hk_ts[f"cal_{this_col}"] = f(hk_ts[this_col])
+
+    record_timeseries(my_hk_ts, "housekeeping", "meddea")
+    create_annotation(my_hk_ts.time[0], f"{hk_ts.meta['ORIGFILE']}", ["meta"])
 
 
 def record_cmd(cmd_ts):
